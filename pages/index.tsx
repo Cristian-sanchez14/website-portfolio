5625a9f021698a77816a1a8e9bda51c5883261b6
Cristian Sanchez <cristian.manu7@gmail.com>
2023-02-28 11:49:55 -0700

    new project

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ import design from "../public/design.png";
 import Image from "next/image";
 import web1 from "../public/portfolio/store-estore.png";
 import web2 from "../public/portfolio/smart-brain.png";
-import web3 from "../public/web3.png";
+import web3 from "../public/portfolio/netflix-img.png";
 import web4 from "../public/portfolio/dotsgame.png";
 import web5 from "../public/portfolio/soccer.png";
 import web6 from "../public/web6.png";
@@ -157,7 +157,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
-              <a href='https://cs-e-commerce.netlify.app/'>
+              <a href='https://cs-e-commerce.netlify.app/' target="_blank" rel="noreferrer">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
@@ -168,7 +168,7 @@ export default function Home() {
               </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <a href='https://smart-brain--frontend.herokuapp.com/'>
+              <a href='https://smart-brain--frontend.herokuapp.com/' target="_blank" rel="noreferrer">
               <Image
                 className="rounded-lg object-cover"
                 layout="responsive"
@@ -178,25 +178,28 @@ export default function Home() {
               </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                layout="responsive"
-                src={web3}
-                alt=''
-              />
+              <a href='https://netflix-reactjs-cristian.netlify.app/' target="_blank" rel="noreferrer">
+                <Image
+                  className="rounded-lg object-cover"
+                  layout="responsive"
+                  src={web3}
+                  alt=''
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <a href='https://63993dab88e78b6f91c08ddc--super-figolla-c0c901.netlify.app/'>
+              <a href='https://63993dab88e78b6f91c08ddc--super-figolla-c0c901.netlify.app/'  target="_blank" rel="noreferrer">
               <Image
                 className="rounded-lg object-cover"
                 layout="responsive"
                 src={web4}
                 alt=''
+                
               />
               </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <a href='https://react-app-soccer.netlify.app/'>
+              <a href='https://react-app-soccer.netlify.app/' target="_blank" rel="noreferrer">
               <Image
                 className="rounded-lg object-cover"
                 layout="responsive"
